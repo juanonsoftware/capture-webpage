2cd4e022a01c5ec8a017a745e9c0b448cf4c45a3
Huan <hhoangvan@pentalog.fr>
2014-08-19 10:37:23 +0700

    Starting capture webpage

---
+++ CaptureWebPage/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CaptureWebPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+            //byte[] img = new Uri("http://www.uol.com.br").ToImage();
+            //using (var fs = new FileStream("test.bmp", FileMode.CreateNew))
+            //{
+            //    fs.Write(img, 0, img.Length);
+            //    fs.Flush();
+            //}
+
+            //WebsiteToImage websiteToImage = new WebsiteToImage("http://stackoverflow.com/questions/2715385/convert-webpage-to-image-from-asp-net", @"Test2.jpg");
+            //websiteToImage.Generate();
+            //WebsiteToImage websiteToImage = new WebsiteToImage("https://github.com/jdavis?tab=repositories", @"Test3.jpg");
+            //websiteToImage.Generate();
+            WebsiteToImage websiteToImage = new WebsiteToImage("http://www.bbc.co.uk/vietnamese/world/2014/08/140817_ukraine_russian_aid_arrives.shtml", @"Test4.jpg");
+            websiteToImage.Generate();
+        }
+    }
+}
